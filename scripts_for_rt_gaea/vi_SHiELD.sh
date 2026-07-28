f2d3014ed41678e358d49597ebaaae599c0b88ef
matt-morin <matthew.morin@noaa.gov>
2026-07-28 15:29:32 +0000

    Cosmetic mod.

--- scripts_for_rt_gaea/vi_SHiELD.sh
+++ scripts_for_rt_gaea/vi_SHiELD.sh
@@ -13,7 +13,6 @@
 # =================================================
 # ${HOME}/NGGPS/VI/VI_scripts/scripts_for_rt_gaea/vi_SHiELD.sh
 #   --- Created by Kun Gao and maintained by Matt Morin (UCAR/GFDL)
-#   --- This script...
 #
 # USAGE:
 #   --- Launched by submit_vi_SHiELD.csh
